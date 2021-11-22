f8ccc4f81b04ebfa2f2965004f45ff503d29a860
Jacob-Max-James <94825791+Jacob-Max-James@users.noreply.github.com>
2021-11-22 12:24:43 +0500

    Add files via upload

---
+++ codebook.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Code Book</title>
+</head>
+<body bgcolor="grey">
+<center>
+ <h1> Code Book </h1>
+</center>
+
+
+<center>
+<form action="login.php" method="post" enctype="multipart/form-data">
+Username : <input type="username" name="username">
+<br>
+Password : <input type="password" name="password">
+<br>
+Login <input type="submit">
+</form>
+</center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<body>
+<html>
